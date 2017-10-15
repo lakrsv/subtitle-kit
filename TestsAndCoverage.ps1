4b7442630a6f9e211aa7d2c13f524b11c81e870c
lakrsv <lakrsv@gmail.com>
2017-10-15 14:08:59 +0100

    Add codecov in favor of coveralls

--- TestsAndCoverage.ps1
+++ TestsAndCoverage.ps1
@@ -2,13 +2,19 @@ $testProjects = "SubtitleKitLibTests"
 
 # Get the most recent OpenCover NuGet package from the dotnet nuget packages
 $nugetOpenCoverPackage = Join-Path -Path "C:\Users\lakrs" -ChildPath "\.nuget\packages\openCover"
+
 $latestOpenCover = Join-Path -Path ((Get-ChildItem -Path $nugetOpenCoverPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\OpenCover.Console.exe"
 # Get the most recent OpenCoverToCoberturaConverter from the dotnet nuget packages
 $nugetCoberturaConverterPackage = Join-Path -Path "C:\Users\lakrs" -ChildPath "\.nuget\packages\opencovertocoberturaconverter"
 $latestCoberturaConverter = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "tools\OpenCoverToCoberturaConverter.exe"
+
 # Get the most recent Coveralls-net package from the dotnet nuget packages
-$nugetCoverallsPackage = Join-Path -Path "C:\users\lakrs" -ChildPath "\.nuget\packages\coveralls.net"
-$latestCoveralls = Join-Path -Path ((Get-ChildItem -Path $nugetCoverallsPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\csmacnz.Coveralls.exe"
+#$nugetCoverallsPackage = Join-Path -Path "C:\users\lakrs" -ChildPath "\.nuget\packages\coveralls.net"
+#$latestCoveralls = Join-Path -Path ((Get-ChildItem -Path $nugetCoverallsPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\csmacnz.Coveralls.exe"
+
+#Get the most recent codecov-exe package for the dotnet nuget packages
+$nugetCodecovPackage = Join-Path -Path "C:\Users\lakrs" -ChildPath "\.nuget\packages\codecov"
+$latestCodecov = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "tools\codecov.exe"
 
 If (Test-Path "$PSScriptRoot\OpenCover.coverageresults"){
 	Remove-Item "$PSScriptRoot\OpenCover.coverageresults"
@@ -47,5 +53,8 @@ foreach ($testProject in $testProjects){
     -output:"$PSScriptRoot\Cobertura.coverageresults" `
     "-sources:$PSScriptRoot"
 	
-"Publishing test results to Coveralls.io"
-& $latestCoveralls --opencover --i "$PSScriptRoot\OpenCover.coverageresults" --useRelativePaths
+#"Publishing test results to Coveralls.io"
+#& $latestCoveralls --opencover --i "$PSScriptRoot\OpenCover.coverageresults" --useRelativePaths
+
+"Publishing test results to Codecov"
+& lastestCodecov -f "$PSScriptRoot\OpenCover.coverageresults" -f $Env:CodeCovToken
